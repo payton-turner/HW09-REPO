5c8bd568641dcfb460201d6e13a90a81025cd7bd
Payton Turner <wdc9um@virginia.edu>
2025-03-23 21:14:49 -0400

    please work

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='Hw09 for booklover',
+    url='https://github.com/payton-turner/HW09-REPO.git',
+    author='Payton Turner',
+    author_email='wdc9um@virginia.edu',
+    license='MIT',
+    packages=['booklover']
+)
